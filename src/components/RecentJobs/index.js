b175e7d6f25d95068e28dc3b3f1ae0cb500377e2
Laura Ramos <lauraramos.globant@latam.com>
2021-04-25 18:44:23 -0500

    change Recent Jobs to show them dinamically

--- src/components/RecentJobs/index.js
+++ src/components/RecentJobs/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import jobs from '../../assets/data/jobs.json'
 
 const RecentJobs = () => (
   <section>
@@ -11,87 +12,26 @@ const RecentJobs = () => (
       </header>
 
       <div className="row item-blocks-connected">
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-google.jpg" alt="" />
-              <div className="hgroup">
-                <h4>Senior front-end developer</h4>
-                <h5>Google</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Menlo park, CA</span>
-                <span className="label label-success">Full-time</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-linkedin.png" alt="" />
-              <div className="hgroup">
-                <h4>Software Engineer (Entry or Senior)</h4>
-                <h5>Linkedin</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Livermore, CA</span>
-                <span className="label label-warning">Part-time</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-envato.png" alt="" />
-              <div className="hgroup">
-                <h4>Full Stack Web Developer</h4>
-                <h5>Envato</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">San Francisco, CA</span>
-                <span className="label label-info">Freelance</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-facebook.png" alt="" />
-              <div className="hgroup">
-                <h4>Web Applications Developer</h4>
-                <h5>Facebook</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Lexington, MA</span>
-                <span className="label label-danger">Internship</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="/img/logo-microsoft.jpg" alt="" />
-              <div className="hgroup">
-                <h4>Sr. SQL Server Developer</h4>
-                <h5>Microsoft</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Palo Alto, CA</span>
-                <span className="label label-success">Remote</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
+        {jobs.map((job) => (
+          <div className="row item-blocks-connected" key={job.id}>
+            <div className="col-xs-12">
+              <a className="item-block" href="/">
+                <header>
+                  <img src={job.image} alt={job.company} />
+                  <div className="hgroup">
+                    <h4>{job.title}</h4>
+                    <h5>{job.company}</h5>
+                  </div>
+                  <div className="header-meta">
+                    <span className="location">{job.location}</span>
+                    <span className="label label-success">{job.type}</span>
+                  </div>
+                </header>
+              </a>
+            </div>
+          </div>
+        ))
+        }
       </div>
 
       <br /><br />
