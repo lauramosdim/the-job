3b199c8dd0d60d25e3e194d35810cdaaf5cd7595
Laura Ramos <lauraramos.globant@latam.com>
2021-04-25 15:49:45 -0500

    change navigation Class component to a functional component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,68 +1,67 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
-class Navigation extends Component {
-  constructor() {
-    super();
+const Navigation = () => {
 
-    this.state = {};
-  }
+    const [logged, setLogged] = useState({ isAuth: false })
 
-  componentWillMount() {
-    this.verifyLogin();
-  }
+    useEffect(() => {
+        verifyLogin()
+    })
 
-  verifyLogin() {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.name) {
-      this.setState({ isAuth: true });
+    const verifyLogin = () => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user && user.name) {
+            setLogged({ isAuth: true });
+        }
     }
-  }
-  render() {
+
     return (
-      <nav className="navbar">
-        <div className="container">
-          <div className="pull-left">
-            <Link className="navbar-toggle" to="/" data-toggle="offcanvas">
-              <i className="ti-menu" />
-            </Link>
-            <div className="logo-wrapper">
-              <Link className="logo" to="/">
-                <img src="/img/logo.png" alt="logo" />
-              </Link>
-              <Link className="logo-alt" to="/">
-                <img src="/img/logo-alt.png" alt="logo-alt" />
-              </Link>
-            </div>
-          </div>
-          <div className="pull-right user-login">
-            <Link to="/login" className="btn btn-sm btn-primary">login</Link>
+        <>
+            <nav className="navbar">
+                <div className="container">
+                    <div className="pull-left">
+                        <Link className="navbar-toggle" to="/" data-toggle="offcanvas">
+                            <i className="ti-menu" />
+                        </Link>
+                        <div className="logo-wrapper">
+                            <Link className="logo" to="/">
+                                <img src="/img/logo.png" alt="logo" />
+                            </Link>
+                            <Link className="logo-alt" to="/">
+                                <img src="/img/logo-alt.png" alt="logo-alt" />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="pull-right user-login">
+                        <Link to="/login" className="btn btn-sm btn-primary">login</Link>
             or <Link to="/register">register</Link>
-          </div>
-          <ul className="nav-menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <a href="/jobs">Position</a>
-              <ul>
-                <li><Link to="/jobs">Browse jobs</Link></li>
-                <li><Link to="/jobs/detail/1020">Job detail</Link></li>
-                <li><Link to="/jobs/apply/1020">Apply for job</Link></li>
-                {/* {
+                    </div>
+                    <ul className="nav-menu">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <a href="/jobs">Position</a>
+                            <ul>
+                                <li><Link to="/jobs">Browse jobs</Link></li>
+                                <li><Link to="/jobs/detail/1020">Job detail</Link></li>
+                                <li><Link to="/jobs/apply/1020">Apply for job</Link></li>
+                                {/* {
                   this.state.isAuth && (
                     <li><Link to="/jobs/create">Post a job</Link></li>
                   )
                 } */}
-                <li><Link to="/jobs/create">Post a job</Link></li>
-                <li><Link to="/candidates">Candidates</Link></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+                                <li><Link to="/jobs/create">Post a job</Link></li>
+                                <li><Link to="/candidates">Candidates</Link></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+
 }
 
-export default Navigation;
+export default Navigation
